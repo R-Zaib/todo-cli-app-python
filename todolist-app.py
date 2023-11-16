41b8922121ef3d18897276457d5aa83415da3434
Don <don@Dons-MacBook-Air.local>
2023-11-16 02:45:58 +0100

    Created basic skeleton of the main() function and added list_task()
    assigned a variable to tasks.txt file to fetch tasks
    
    defined a list_tasks() function to list tasks from .txt file with indexes

--- todolist-app.py
+++ todolist-app.py
@@ -1,3 +1,51 @@
 import sys
 
-# creating a todo list app on CLI using python
+# reference: https://www.pythontutorial.net/python-basics/python-read-text-file/
+# the above reference can be used for read 'r', write 'w' and append 'a'
+
+read_tasks = open("tasks.txt", "r")
+#   print(read_tasks.readlines())          # only for testing
+
+# reference: https://stackoverflow.com/questions/64255055/how-to-use-the-enumerate-function-on-a-txt-file
+def list_tasks():
+    tasks = read_tasks.readlines()
+    if tasks:
+        for i, task in enumerate(tasks, 1):
+            print(i, task)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Please enter correct number of arguments")
+    elif sys.argv[1] == "-l":
+        list_tasks()
+    else:
+        print("Error! Enter the correct arguments. Use '-l' to list tasks.")
+
+
+if __name__ == "__main__":
+    main()
+    
+
+# welcome_message = """
+# Welcome to To Do List App!
+
+# ==============================
+
+# Command-line arguments:
+
+# -l Lists all the tasks
+
+# -a Adds a new task
+
+# -r Removes a task
+
+# -c Completes a task
+# """
+
+# print(welcome_message)
+
+# list_tasks()      # for testing
+
+# click library for handling command line 
+# argparse is also good for it
+# functions to create --> add_task, list_tasks, remove_task, complete_task, read_tasks
