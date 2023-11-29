1dea2e4b9b6f05b2bbc7d26da96a3e94fee1c691
Don <don@Dons-MacBook-Air.local>
2023-11-30 00:07:32 +0100

    removed the empty line error by updating read_task_file_lines function and fixed the join in write_task_file_lines funciton
    fixed the max index value, and updated in main functino.
    
    fixed the printing of the removed task, to exclude the brackets in removed_task function
    
    changed the complete_task function to not consider brackets added by user
    
    removed the printing of list after each command is run, e.g complete task, add task or remove task in the main function
    
    changed f' string back to normal string.

--- todolist-app.py
+++ todolist-app.py
@@ -7,9 +7,8 @@ import sys
 
 FILE_PATH = "tasks.txt"
 MIN_INDEX_VALUE = 1
-MAX_INDEX_VALUE = len("tasks.txt")
-ARG_INDEX = sys.argv
-ARG_LENGTH = len(ARG_INDEX)
+ARG_COMMAND = sys.argv
+ARG_LENGTH = len(ARG_COMMAND)
 CMD_LIST = "-l"
 CMD_ADD = "-a"
 CMD_REMOVE = "-r"
@@ -30,10 +29,16 @@ Command-line arguments:
 -c Completes a task
 """
 
+
 def read_task_file_lines():
     try:
+        tasks_todo_list = []
         with open(FILE_PATH, "r") as read_tasks:
-            return read_tasks.readlines()
+            for row in read_tasks:
+                r = row.strip()
+                if r:
+                    tasks_todo_list.append(r)    
+        return tasks_todo_list
     except FileNotFoundError:
         print("Error! Task file not found. Please create 'tasks.txt' file.")
 
@@ -41,47 +46,57 @@ def read_task_file_lines():
 def write_task_file_lines(lines):
     try:
         with open(FILE_PATH, "w") as w_tasks:
-            w_tasks.writelines(lines)
+            w_tasks.write("\n".join(lines))
     except FileNotFoundError:
         print("Error! Task file not found. Please create 'tasks.txt' file.")
+    except Exception as e:
+        print("An error occurred. Error type: ",e)
 
 
 def list_tasks():
     tasks = read_task_file_lines()
     if tasks:
-        for i, task in enumerate(tasks, 1):
-            print(i, "-", task)
+        for i, task in enumerate(tasks, MIN_INDEX_VALUE):
+            print(i, "-", task.rstrip())
     else:
         print("No todo tasks for today!")
 
+
+# reference: https://sparkbyexamples.com/python/python-remove-a-trailing-new-line/
 def add_task(task):
     try:
         with open(FILE_PATH, "a") as a_task:
-            a_task.write("\n" + task)
+            a_task.write("\n" + "[ ] " + task.rstrip())
     except FileNotFoundError:
         print("Error! Task file not found. Please create 'tasks.txt' file.")
     except Exception as e:
-        print(f"An error occurred while adding the task: {e}")
+        print("An error occurred. Error type: ",e)
 
 
 # reference: https://datagy.io/python-list-pop-remove-del-clear/ 
+# reference: https://stackoverflow.com/questions/60607645/replacing-characters-by-chaining-str-replace-methods-produces-wrong-result
 def remove_task(index):
     tasks = read_task_file_lines()
-    if MIN_INDEX_VALUE < index <= MAX_INDEX_VALUE:
+    if tasks and MIN_INDEX_VALUE <= index <= len(tasks):
         task_removed = tasks.pop(index - 1)
         write_task_file_lines(tasks)
-        print("Removed task: " + task_removed)
+        print_removed_task = task_removed.replace("[ ] ", "").replace("[\u2713]", "")
+        print("Removed task: " + print_removed_task)
     else:
         print("Error! Invalid index, no task was removed.")
 
 
+# reference: https://stackoverflow.com/questions/41079054/how-does-python-startswith-work
 def complete_task(index):
     tasks = read_task_file_lines()
-    if MIN_INDEX_VALUE < index <= MAX_INDEX_VALUE:
-        tasks[index - 1] = u'\u2713' + " " + tasks[index - 1]
-        # tasks.insert(-1, u'\u2713')
-        write_task_file_lines(tasks)
-        print("Marked", index, "as completed.")
+    if tasks and MIN_INDEX_VALUE <= index <= len(tasks):
+        i = index - 1
+        if "[\u2713] " in tasks[i]:
+            print("Task", index, "is already marked as completed.")
+        elif "[ ]" in tasks[i]:
+            tasks[i] = tasks[i].replace("[ ] ", "[\u2713] ", 1)
+            write_task_file_lines(tasks)
+            print("Marked", index, "as completed.")
     else:
         print("Error! Invalid index, no task was completed.")
 
@@ -89,30 +104,27 @@ def complete_task(index):
 def main():
     if ARG_LENGTH == 1:
         print(WELCOME_MESSAGE) 
-    elif ARG_LENGTH == 2 and ARG_INDEX[1] == CMD_LIST:
+    elif ARG_LENGTH == 2 and ARG_COMMAND[1] == CMD_LIST:
         list_tasks()
-    elif ARG_LENGTH > 2 and ARG_INDEX[1] == CMD_ADD:
-        new_task_to_add = ' '.join(ARG_INDEX[2:])
+    elif ARG_LENGTH > 2 and ARG_COMMAND[1] == CMD_ADD:
+        new_task_to_add = ' '.join(ARG_COMMAND[2:])
         if not new_task_to_add:
             print("Error! Unable to add new task, no task given!")   
         else:
             add_task(new_task_to_add)
             print("New task: ", new_task_to_add, "added.")
-            list_tasks()
-    elif ARG_LENGTH == 3 and ARG_INDEX[1] == CMD_REMOVE:
-        index_to_remove = int(ARG_INDEX[2])
+    elif ARG_LENGTH == 3 and ARG_COMMAND[1] == CMD_REMOVE:
+        index_to_remove = int(ARG_COMMAND[2])
         if not index_to_remove:
             print("Error: No task index was entered to remove task.")
         else:
             remove_task(index_to_remove)
-            list_tasks()
-    elif ARG_LENGTH == 3 and ARG_INDEX[1] == CMD_COMPLETE:
-        task_index_to_complete = int(ARG_INDEX[2])
+    elif ARG_LENGTH == 3 and ARG_COMMAND[1] == CMD_COMPLETE:
+        task_index_to_complete = int(ARG_COMMAND[2])
         if not task_index_to_complete:
             print("Error! No task index was entered to complete task.")
         else:
             complete_task(task_index_to_complete)
-            list_tasks()
     else:
         print("Error! Enter the correct arguments.")
 
@@ -123,6 +135,4 @@ if __name__ == "__main__":
     except ValueError:
         print("Error! Please enter the correct value")    
     except Exception as e:
-        print(f"An error occurred. Error type: {e}")
-
-
+        print("An error occurred. Error type: ",e)
